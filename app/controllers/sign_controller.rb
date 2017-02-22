275fef71a6173888a1d65c74001023a2cdca8a9b
lxd <i@lxd.me>
2017-02-22 15:34:02 +0800

    Verify openid with access_token

--- app/controllers/sign_controller.rb
+++ app/controllers/sign_controller.rb
@@ -11,8 +11,19 @@ class SignController < ApplicationController
       @user = User.new(phone: params[:phone], air_auth_token: SecureRandom.uuid) if @user.nil?
 
     elsif params.has_key?(:wechat_openid)
+      params.require(:wechat_access_token)
+
+      url = URI.parse("https://api.weixin.qq.com/cgi-bin/user/info?access_token=#{params[:wechat_access_token]}&openid=#{params[:wechat_openid]}&lang=en_US")
+      req = Net::HTTP::Get.new(url.to_s)
+      res = Net::HTTP.start(url.host, url.port, use_ssl: true) {|http|
+        http.request(req)
+      }
+      res_json = JSON.parse(res.body)
+
+      render json: res_json, status: 400 and return if res_json.has_key? 'errcode'
+
       @user = User.find_by(wechat_openid: params[:wechat_openid])
-      @user = User.new(wechat_openid: params[:wechat_openid], air_auth_token: SecureRandom.uuid) if @user.nil?
+      @user = User.new(wechat_openid: params[:wechat_openid], air_auth_token: SecureRandom.uuid, wechat_userinfo: res_json) if @user.nil?
 
     else
       logger.error 'Client sign with either phone or wechat'
@@ -20,7 +31,6 @@ class SignController < ApplicationController
     end
 
     @user.save! if @user[:id].nil?
-
     render json: { air_auth_token: @user[:air_auth_token] }
   end
 end
