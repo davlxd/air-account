145052909f7e4dbe6bd51385f1510cb777651f23
lxd <i@lxd.me>
2017-02-25 22:49:32 +0800

    Refactor, fix some issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post '/sign', to: 'sign#sign'
-  post '/sms-ver-code', to: 'sms_ver_code#push'
+  post '/account/sign', to: 'sign#sign'
+  post '/account/sms-ver-code', to: 'sms_ver_code#push'
 end
